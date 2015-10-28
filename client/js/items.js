76995590a10afe2f568a7c8e286e63d3e88fbc4b
Mauricio Navarro Miranda <mauricio@navarromiranda.mx>
2015-10-27 19:22:25 -0600

    becu-client

---
+++ client/js/items.js
@@ -0,0 +1,117 @@
+
+define(['item'], function(Item) {
+
+    var Items = {
+
+        Sword2: Item.extend({
+            init: function(id) {
+                this._super(id, Types.Entities.SWORD2, "weapon");
+                this.lootMessage = "Obtuviste una espada de acero";
+            },
+        }),
+
+        Axe: Item.extend({
+            init: function(id) {
+                this._super(id, Types.Entities.AXE, "weapon");
+                this.lootMessage = "Obtuviste un hacha";
+            },
+        }),
+
+        RedSword: Item.extend({
+            init: function(id) {
+                this._super(id, Types.Entities.REDSWORD, "weapon");
+                this.lootMessage = "Obtuviste una espada en llamas";
+            },
+        }),
+
+        BlueSword: Item.extend({
+            init: function(id) {
+                this._super(id, Types.Entities.BLUESWORD, "weapon");
+                this.lootMessage = "Obtuviste una espada mágica";
+            },
+        }),
+
+        GoldenSword: Item.extend({
+            init: function(id) {
+                this._super(id, Types.Entities.GOLDENSWORD, "weapon");
+                this.lootMessage = "Obtuviste la espada máxima";
+            },
+        }),
+
+        MorningStar: Item.extend({
+            init: function(id) {
+                this._super(id, Types.Entities.MORNINGSTAR, "weapon");
+                this.lootMessage = "Obtuviste la estrella de la mañana";
+            },
+        }),
+
+        LeatherArmor: Item.extend({
+            init: function(id) {
+                this._super(id, Types.Entities.LEATHERARMOR, "armor");
+                this.lootMessage = "Tienes armadura de cuero";
+            },
+        }),
+
+        MailArmor: Item.extend({
+            init: function(id) {
+                this._super(id, Types.Entities.MAILARMOR, "armor");
+                this.lootMessage = "Tienes armadura de malla";
+            },
+        }),
+
+        PlateArmor: Item.extend({
+            init: function(id) {
+                this._super(id, Types.Entities.PLATEARMOR, "armor");
+                this.lootMessage = "Tienes armadura de metal";
+            },
+        }),
+
+        RedArmor: Item.extend({
+            init: function(id) {
+                this._super(id, Types.Entities.REDARMOR, "armor");
+                this.lootMessage = "Tienes armadura de rubí";
+            },
+        }),
+
+        GoldenArmor: Item.extend({
+            init: function(id) {
+                this._super(id, Types.Entities.GOLDENARMOR, "armor");
+                this.lootMessage = "Tienes armadura de oro";
+            },
+        }),
+
+        Flask: Item.extend({
+            init: function(id) {
+                this._super(id, Types.Entities.FLASK, "object");
+                this.lootMessage = "Bebiste una poción de salud";
+            },
+        }),
+
+        Cake: Item.extend({
+            init: function(id) {
+                this._super(id, Types.Entities.CAKE, "object");
+                this.lootMessage = "Comiste un pastel";
+            },
+        }),
+
+        Burger: Item.extend({
+            init: function(id) {
+                this._super(id, Types.Entities.BURGER, "object");
+                this.lootMessage = "You can haz rat burger";
+            },
+        }),
+
+        FirePotion: Item.extend({
+            init: function(id) {
+                this._super(id, Types.Entities.FIREPOTION, "object");
+                this.lootMessage = "¡Siente el poder de Firefox!";
+            },
+
+            onLoot: function(player) {
+                player.startInvincibility();
+            },
+        }),
+    };
+
+    return Items;
+});
